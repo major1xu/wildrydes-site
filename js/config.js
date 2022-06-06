d039c5461283d1b47f5532b453a6e2d4bf34da0a
Minjie Xu <minjiexu@mini-de-minjie.lan>
2022-06-06 15:07:52 -0500

    update cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_E6qE5kDcD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3gmqevl5ed3t1gk6gijp4lbb78', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
