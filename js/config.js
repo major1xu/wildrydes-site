7560397ea19843cd0598649fce58fa8fd83bf685
Minjie Xu <minjiexu@mini-de-minjie.lan>
2022-06-06 15:13:40 -0500

    update cognito 2nd try

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_E6qE5kDcD', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3gmqevl5ed3t1gk6gijp4lbb78', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us_east_1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
