422ccd1896e658784669995ecb609cb5b5f8e54a
Minjie Xu <minjiexu@mini-de-minjie.lan>
2022-06-06 21:03:24 -0500

    update api: invoke url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us_east_1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qlbt8h3by4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
